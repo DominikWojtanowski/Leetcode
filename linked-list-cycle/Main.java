5bf78ba99ac7685b4ea34d6aa2294b687a321940
Dominik Wojtanowski <112193459+DominikWojtanowski@users.noreply.github.com>
2026-09-17 19:37:31 +0200

    linked-list-cycle

---
+++ linked-list-cycle/Main.java
@@ -0,0 +1,34 @@
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (slow != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
